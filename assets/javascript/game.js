824a89376a03e3e2c32bcce446b299336ce5ae21
SarahHei <sarahhei@compasseval.com>
2018-10-08 19:12:54 -0400

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,88 +2,94 @@ $(document).ready(function() {
     
 var wins = 0;
 var losses = 0;
-var targetNumber = 0;
-var randomX = 19;
 var totalScore = 0;
-var b1 = 0;
-var b2 = 0;
-var b3 = 0;
-var b4 = 0;
-
-// console.log(totalScore);
-
-$("#start").on("click", function() {
-    targetNumber = 0;
-    randomX = [Math.floor(Math.random() * 120)];
-    targetNumber = randomX;
+var b1;
+var b2;
+var b3;
+var b4;
+targetNumber = [Math.floor(Math.random() * 120) + 19];
+    b1 = Math.floor(Math.random() *12) +1;
+    b2 = Math.floor(Math.random() *12) +1;
+    b3 = Math.floor(Math.random() *12) +1;
+    b4 = Math.floor(Math.random() *12) +1;
     $("#targetNumber").append(targetNumber);
-})
 
 $("#button1").on("click", function () {
-    if (b1) {
-        totalScore = totalScore + b1;
-    }
-    else {
-        b1 = Math.floor(Math.random() * 12);
-    } 
-    $("#totalScore").text("Your Total Score so far is: "+ totalScore);
+    totalScore = totalScore + b1;
+    $("#totalScore").text("Here is your Total Score so far: " + totalScore);
     console.log(b1);
-}
-);
-
-$("#button2").on("click", function () {
-    if (b2) {
-        totalScore = totalScore + b2;
-    }
-    else {
-        b2 = Math.floor(Math.random() * 12);
-    } 
-    $("#totalScore").text("Your Total Score so far is: "+ totalScore);
-    console.log(b2);
-}
-);
-
-$("#button3").on("click", function () {
-    if (b3) {
-        totalScore = totalScore + b3;
-    }
-    else {
-        b3 = Math.floor(Math.random() * 12);
-    } 
-    $("#totalScore").text("Your Total Score so far is: "+ totalScore);
-    console.log(b3);
-}
-);
-
-$("#button4").on("click", function () {
-    if (b4) {
-        totalScore = totalScore + b4;
-    }
-    else {
-        b4 = Math.floor(Math.random() * 12);
-    } 
-    $("#totalScore").text("Your Total Score so far is: "+ totalScore);
-    console.log(b4);
-}
-);
-
-if (totalScore === targetNumber) {
+    if (totalScore === targetNumber) {
      wins++;
      console.log(wins);
-     $("#wins").html("Here are your wins so far: " + wins);
+     $("#wins").text("Here are your wins so far: " + wins);
      }
     else if (totalScore > targetNumber){
     losses++;
     console.log(losses);
-    $("#losses").html("Here are your losses so far: " + losses);
+    $("#losses").text("Here are your losses so far: " + losses);
     }
+});
 
+$("#button2").on("click", function () {
+    totalScore = totalScore + b2;
+    $("#totalScore").text("Here is your Total Score so far: " + totalScore);
+    console.log(b2);
+    if (totalScore === targetNumber) {
+        wins++;
+        console.log(wins);
+        $("#wins").text("Here are your wins so far: " + wins);
+        }
+       else if (totalScore > targetNumber){
+       losses++;
+       console.log(losses);
+       $("#losses").text("Here are your losses so far: " + losses);
+       }
+}
+);
+
+$("#button3").on("click", function () {
+    totalScore = totalScore + b3;
+    $("#totalScore").text("Here is your Total Score so far: " + totalScore);
+    console.log(b3);
+    if (totalScore === targetNumber) {
+        wins++;
+        console.log(wins);
+        $("#wins").text("Here are your wins so far: " + wins);
+        }
+       else if (totalScore > targetNumber){
+       losses++;
+       console.log(losses);
+       $("#losses").text("Here are your losses so far: " + losses);
+       }
+}
+);
+
+$("#button4").on("click", function () {
+    totalScore = totalScore + b4;
+    $("#totalScore").text("Here is your Total Score so far: " + totalScore);
+    console.log(b4);
+    if (totalScore === targetNumber) {
+        wins++;
+        console.log(wins);
+        $("#wins").text("Here are your wins so far: " + wins);
+        }
+       else if (totalScore > targetNumber){
+       losses++;
+       console.log(losses);
+       $("#losses").text("Here are your losses so far: " + losses);
+       }
+}
+);
 
 $("#reset").on("click", function() {
-    targetNumber="";
+    targetNumber = [Math.floor(Math.random() * 120) + 19];
     $("#targetNumber").html(targetNumber);
+    totalScore =0;
+    $("#totalScore").html("Here is your Total Score: " + totalScore);
+    b1 = Math.floor(Math.random() *12);
+    b2 = Math.floor(Math.random() *12);
+    b3 = Math.floor(Math.random() *12);
+    b4 = Math.floor(Math.random() *12);
 })
-
-
 
 });
